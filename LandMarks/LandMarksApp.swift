6832963417d5fb11fba496df29b67aeece186b68
Poudja CANESSANE <poudja.canessane@icloud.com>
2021-03-17 17:35:14 +0100

    Creating and combining views

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Canessane Poudja on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
